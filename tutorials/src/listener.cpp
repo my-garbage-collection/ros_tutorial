f61ed1aa6f588fda712738a59b2075d952e8871f
mu-777 <ryosuke.murata.7@gmail.com>
2015-04-09 12:56:00 +0900

    add yozakura_display

---
+++ tutorials/src/listener.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
+/**
+ * This tutorial demonstrates simple receipt of messages over the ROS system.
+ */
+void chatterCallback(const std_msgs::String::ConstPtr& msg) {
+    ROS_INFO("I heard: [%s]", msg->data.c_str());
+    //std_msgs::String::Ptr test = &msg + 1;
+    //std::cout<< msg<<std::endl;
+    //std::cout<< (msg << sizeof(int))<<std::endl;
+    //std::cout<< test->data.c_str() <<std::endl;
+}
+
+int main(int argc, char** argv) {
+
+    ros::init(argc, argv, "listener");
+    ros::NodeHandle n;
+    ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
+    ros::spin();
+
+    return 0;
+}
